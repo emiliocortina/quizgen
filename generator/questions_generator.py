8ad96fe03fb35332d8da73aa97f64f3f6474b49c
Emilio <emiliocortina@gmail.com>
2019-11-22 11:25:12 +0100

    Fixing error when using distinct with several fields

--- generator/questions_generator.py
+++ generator/questions_generator.py
@@ -3,6 +3,7 @@ import requests
 from requests.exceptions import ReadTimeout
 from tfg.categories.manager import getEntityCategory, getCategoryQuestions, isValidCategory
 
+
 def generateQuestions(entity_id, n_questions=5, questions_category='Q52511956', locale="en"):
     if not isValidCategory(questions_category):
         return ['Please enter a valid category']
@@ -26,7 +27,7 @@ def generateQuestion(entity_id, property_id, statement, locale):
         print('Correct Answer: ', correct_answer['answerEntityLabel']['value'])
         distractors = getDistractors(property_id)
         for distractor in distractors:
-            print(distractor['distractorLabel']['value'])
+            print(distractor['distractorEntityLabel']['value'])
         return {'statement': statement, 'answer': correct_answer, 'distractors': distractors}
     except IndexError:
         print('Data not found for given property and entity')
@@ -35,6 +36,7 @@ def generateQuestion(entity_id, property_id, statement, locale):
     else:
         print('Some error occurred')
     return None
+
 
 def getEntityLabel(entity_id, locale):
     query = """
@@ -50,40 +52,55 @@ def getEntityLabel(entity_id, locale):
 def getCorrectAnswer(entity_id, property_id):
     query = """
     SELECT
-      ?answerEntity ?answerType ?answerEntityLabel
+      ?answerEntity (SAMPLE(?type) AS ?answerType) (SAMPLE(?label) AS  ?answerEntityLabel)
     WHERE {
-       wd:%s wdt:%s ?answerEntity.
-       OPTIONAL {?answerEntity wdt:P31 ?answerType.}
-
+      wd:%s wdt:%s ?answerEntity.
+      ?answerEntity rdfs:label ?label . 
+      OPTIONAL {?answerEntity wdt:P31 ?type.}
+      FILTER (langMatches( lang(?label), "en" ) )
       SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
     }
+    GROUP BY ?answerEntity
     """ % (entity_id, property_id)
     results = makeQuery(query)
     return results[0]
 
 
 def getDistractors(property_id):
-    query = """
-    SELECT DISTINCT ?distractorEntity ?distractorType ?distractorLabel
+    entitiesQuery = """
+    SELECT DISTINCT ?distractorEntity ?distractorEntityLabel
     WHERE {
       ?subject wdt:%s ?distractorEntity.
-      OPTIONAL {?distractorLabel wdt:P31 ?distractorType.}
 
       SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
     }
     LIMIT 3
-    """ % (property_id)
-    results = makeQuery(query)
-    return results
+    """ % property_id
+    typesQuery = """
+    SELECT DISTINCT ?distractorEntityType
+    WHERE {
+      wd:%s wdt:P31 ?distractorEntityType.
+    }
+    LIMIT 5
+    """
+    entities = makeQuery(entitiesQuery)
+
+    for entity in entities:
+        entity_id = entity['distractorEntity']['value'].split('/')[-1]
+        query = typesQuery % entity_id
+        types = makeQuery(query)
+        entity['distractorEntityTypes'] = types[0]
+        pass
+    return entities
 
 
 def makeQuery(query):
     url = 'https://query.wikidata.org/sparql'
-    r = requests.get(url, params={'format': 'json', 'query': query}, timeout=20)
+    r = requests.get(url, params={'format': 'json', 'query': query}, timeout=60)
     while r.status_code == 429:
         time.sleep(1.2)
         r = requests.get(url, params={'format': 'json', 'query': query})
     if r.status_code != 200:
-        print('Error')
+        raise IndexError()
     data = r.json()
     return data['results']['bindings']
